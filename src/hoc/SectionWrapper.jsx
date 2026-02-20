0dab4b01a21c51694937a4551b592177008f9653
hanahumaira <hhumaira2209@gmail.com>
2026-02-20 13:27:07 +0800

    fix earth

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -8,7 +8,7 @@ const SectionWrapper = (Component, idName) => function HOC() {
       variants={staggerContainer()}
       initial="hidden"
       whileInView="show"
-      viewport={{once: true, amount: 0.1}}
+      viewport={{once: true, amount: 0.25}}
       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
         <span className="hash-span" id={idName}>
